09193760606a42d0c48f0b50b0658459ea295132
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-20 17:24:19 -0500

    added LoginPage page markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
