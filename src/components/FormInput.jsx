ab8d09dcd8ae21766a32abcb9fb7d374edd15b28
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-20 17:34:09 -0500

    added rest to FormInput cmponent

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,6 +1,6 @@
 import { Input, Typography } from "@material-tailwind/react";
 
-const FormInput = ({ placeholder, type = "text", label }) => {
+const FormInput = ({ placeholder, type = "text", label, ...rest }) => {
   return (
     <div className="w-full">
       <Typography
@@ -16,6 +16,7 @@ const FormInput = ({ placeholder, type = "text", label }) => {
         labelProps={{
           className: "hidden",
         }}
+        {...rest}
         className="!border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
       />
     </div>
