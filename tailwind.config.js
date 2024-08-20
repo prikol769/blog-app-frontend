df009a38979efa8811f909cc82eecb29a17b7c47
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-20 17:19:55 -0500

    added material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import withMT from "@material-tailwind/react/utils/withMT";
+
+const config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+const withMaterialTailwind = withMT(config);
+
+export default withMaterialTailwind;
