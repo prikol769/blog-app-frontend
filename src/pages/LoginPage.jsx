359429fad0e3285585c35914446cc53084f7318d
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-20 19:50:48 -0500

    added redirect

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,10 +2,12 @@ import { Button } from "@material-tailwind/react";
 import FormInput from "../components/FormInput";
 import { useState } from "react";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -16,13 +18,21 @@ const LoginPage = () => {
         {
           username,
           password,
-        }
+        },
+        { withCredentials: true }
       );
       console.log(response, "response");
+      if (response.id) {
+        setRedirect(true);
+      }
     } catch (error) {
       console.log(error);
     }
   };
+
+  if (redirect) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div className="flex items-center justify-center">
