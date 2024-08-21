f9e4d1856a7a23d01ee6d7480bf0b5092218edc1
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-20 19:05:35 -0500

    added handleSubmit RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,46 @@
 import { Button } from "@material-tailwind/react";
 import FormInput from "../components/FormInput";
+import { useState } from "react";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data: response } = await axios.post(
+        "http://localhost:5000/register",
+        {
+          username,
+          password,
+        }
+      );
+      console.log(response, "response");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center">
-      <form className="flex flex-col gap-10 w-[500px]">
-        <FormInput placeholder="Username" label="Username" />
-        <FormInput placeholder="Password" type="password" label="Password" />
-        <Button>Register</Button>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-10 w-[500px]">
+        <FormInput
+          placeholder="Username"
+          label="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <FormInput
+          placeholder="Password"
+          type="password"
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="submit">Register</Button>
       </form>
     </div>
   );
