b827e49540cee53cc6726944a509be774951bde9
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-22 16:14:43 -0500

    changed FormInput styles

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -17,7 +17,7 @@ const FormInput = ({ placeholder, type = "text", label, ...rest }) => {
           className: "hidden",
         }}
         {...rest}
-        className="!border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+        className="h-[50px]  !border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
       />
     </div>
   );
