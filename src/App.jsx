e72aff2f892cb108731b37a75ad1ab928c865082
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-22 16:14:25 -0500

    added AuthLayout

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import Layout from "./components/Layout";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import { GlobalProvider } from "./context/GlobalContext";
+import AuthLayout from "./components/AuthLayout";
 
 const App = () => {
   return (
     <>
       <GlobalProvider>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+          <Route path="/" element={<AuthLayout />}>
             <Route path="/sign-in" element={<SignInPage />} />
             <Route path="/sign-up" element={<SignUpPage />} />
+          </Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
           </Route>
         </Routes>
       </GlobalProvider>
