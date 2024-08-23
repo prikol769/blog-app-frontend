8b90b84c52a45064eed6ddaf496aa9ce459cebbd
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-22 19:38:36 -0500

    added Badge component

---
+++ src/components/Badge.jsx
@@ -0,0 +1,9 @@
+const Badge = () => {
+  return (
+    <div className="text-[#4b6bfb98] rounded-md bg-[#2938800d] px-[10px] py-1 w-fit text-xs font-semibold">
+      Technology
+    </div>
+  );
+};
+
+export default Badge;
