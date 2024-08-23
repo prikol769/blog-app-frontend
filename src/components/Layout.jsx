dbf6125a8467c032a3c5909a80e611e6720c9305
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-23 12:24:16 -0500

    added Footer to Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function Layout() {
   return (
-    <div className="max-w-[1400px] mx-auto px-[24px]">
-      <Header />
-      <main>
-        <Outlet />
-      </main>
+    <div>
+      <div className="max-w-[1400px] mx-auto px-[24px]">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 }
