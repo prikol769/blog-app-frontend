6da28db9e2c1949e272794fbd708b79837b57d70
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-25 22:01:59 -0500

    added PreviwPostCard component

---
+++ src/components/PreviwPostCard.jsx
@@ -0,0 +1,46 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+} from "@material-tailwind/react";
+import Badge from "./Badge";
+import { formatDate } from "../utils/dateFormat";
+
+export function PreviwPostCard({ category, title, summary, author }) {
+  const upperCaseTitle = title.charAt(0).toUpperCase() + title.slice(1);
+  return (
+    <Card className="pt-10 max-w-[600px] w-auto min-w-96 shadow-[0px_4px_6px_1px_rgb(0,0,0,0.1)] min-h-[480px] max-h-[480px] justify-between">
+      <CardHeader color="blue-gray" className="relative h-56">
+        <img
+          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+          alt="card-image"
+        />
+      </CardHeader>
+      <CardBody className="min-h-[175px]">
+        {<Badge category={category ? category : "Your category"} />}
+        <h5
+          color="blue-gray"
+          className="mb-2 mt-5 line-clamp-1 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-inherit"
+        >
+          {upperCaseTitle}
+        </h5>
+        <p className="line-clamp-2">{summary}</p>
+      </CardBody>
+      <hr className="mb-5" />
+      <CardFooter className="pt-0 flex items-center justify-between">
+        <div className="flex justify-between items-center w-full">
+          <div className="flex items-center gap-2">
+            <img className="h-8 w-8" src="/hero-avatar.png" alt="hero-avatar" />
+            <div>
+              <p className="text-[#97989F] text-xs font-semibold">{author}</p>
+              <p className="text-[#97989F] text-xs">{formatDate(new Date())}</p>
+            </div>
+          </div>
+          <Button className="w-[50%]">Read More</Button>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -1,9 +1,17 @@
-import { Button, Textarea, Typography } from "@material-tailwind/react";
+import {
+  Button,
+  ButtonGroup,
+  Textarea,
+  Typography,
+} from "@material-tailwind/react";
+
 import FormInput from "../components/FormInput";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
 import axios from "axios";
+import { PreviwPostCard } from "../components/PreviwPostCard";
+import { useGlobalContext } from "../context/GlobalContext";
 
 const modules = {
   toolbar: [
@@ -34,10 +42,14 @@ const formats = [
 ];
 
 const CreatePostPage = () => {
+  const { userInfo } = useGlobalContext();
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
+  const [previewCard, setPreviewCard] = useState(false);
+  const [previewArticle, setPreviewArticle] = useState(false);
+  console.log(previewCard, "previewCard");
 
   const createNewPost = async (e) => {
     e.preventDefault();
@@ -60,55 +72,95 @@ const CreatePostPage = () => {
 
   return (
     <div className="mb-10">
-      <form className="flex flex-col gap-4" onSubmit={createNewPost}>
-        <div className="flex gap-6">
-          <FormInput
-            label="Title"
-            maxLength="200"
-            required
-            placeholder="Technology is our future!"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <FormInput
-            label="Category"
-            required
-            maxLength="15"
-            placeholder="Next.js React JavaScript"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-        </div>
-        <div className="w-full">
-          <Typography
-            variant="small"
-            color="blue-gray"
-            className="mb-2 font-medium"
-          >
-            Summary
-          </Typography>
-          <Textarea
-            labelProps={{
-              className: "hidden",
+      <div className="py-4 flex items-center justify-end">
+        <ButtonGroup variant="outlined">
+          <Button
+            className={`${
+              previewCard ? "bg-[#212121] text-white" : "bg-white text-black"
+            }`}
+            onClick={() => {
+              setPreviewCard(!previewCard);
+              setPreviewArticle(false);
             }}
-            maxLength="400"
-            className=" !border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+          >
+            Card
+          </Button>
+          <Button
+            className={`${
+              previewArticle ? "bg-[#212121] text-white" : "bg-white text-black"
+            }`}
+            onClick={() => {
+              setPreviewArticle(!previewArticle);
+              setPreviewCard(false);
+            }}
+          >
+            Article
+          </Button>
+        </ButtonGroup>
+      </div>
+      <div className={`flex ${previewCard ? "gap-12" : ""}`}>
+        <form className="flex flex-col gap-4 flex-1" onSubmit={createNewPost}>
+          <div className="flex gap-6">
+            <FormInput
+              label="Title"
+              maxLength="200"
+              required
+              placeholder="Technology is our future!"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <FormInput
+              label="Category"
+              required
+              maxLength="15"
+              placeholder="Next.js React JavaScript"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+          </div>
+          <div className="w-full">
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="mb-2 font-medium"
+            >
+              Summary
+            </Typography>
+            <Textarea
+              labelProps={{
+                className: "hidden",
+              }}
+              maxLength="400"
+              className=" !border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+              required
+              placeholder="Provide a brief overview of the key points discussed..."
+              value={summary}
+              onChange={(e) => setSummary(e.target.value)}
+            />
+          </div>
+          <ReactQuill
             required
-            placeholder="Provide a brief overview of the key points discussed..."
-            value={summary}
-            onChange={(e) => setSummary(e.target.value)}
+            modules={modules}
+            formats={formats}
+            value={content}
+            onChange={(newValue) => setContent(newValue)}
+            className="!border !rounded-lg !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 "
           />
+          <Button type="submit">Create Post</Button>
+        </form>
+        <div className="max-w-[430px]">
+          {previewCard && (
+            <div>
+              <PreviwPostCard
+                title={title}
+                category={category}
+                summary={summary}
+                author={userInfo.fullName}
+              />
+            </div>
+          )}
         </div>
-        <ReactQuill
-          required
-          modules={modules}
-          formats={formats}
-          value={content}
-          onChange={(newValue) => setContent(newValue)}
-          className="!border !rounded-lg !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 "
-        />
-        <Button type="submit">Create Post</Button>
-      </form>
+      </div>
     </div>
   );
 };
