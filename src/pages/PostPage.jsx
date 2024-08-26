174604cd64dcd38f8d7bf1647b82b03834ec609f
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-26 13:58:13 -0500

    added PostPage

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,54 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Badge from "../components/Badge";
+import { formatDate } from "../utils/dateFormat";
+
 const PostPage = () => {
-  return <div>PostPage</div>;
+  const { id } = useParams();
+  const [post, setPost] = useState([]);
+  console.log(post);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const { data: response } = await axios(
+          `http://localhost:5000/api/posts/${id}`
+        );
+        console.log(response, "response");
+        setPost(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPost();
+  }, []);
+
+  return (
+    <div className="pt-8 pb-[80px] max-w-[1000px] mx-auto">
+      <Badge category={post.category} />
+      <h1 className="text-[#181A2A] text-4xl font-semibold mt-4 mb-5">
+        {post.title}
+      </h1>
+      <div className="flex items-center gap-6">
+        <div className="flex items-center gap-2">
+          <img className="h-8 w-8" src="/hero-avatar.png" alt="hero-avatar" />
+          <p className="text-[#97989F] text-sm font-semibold">{post.author}</p>
+        </div>
+        <p className="text-[#97989F] text-sm">{formatDate(post.createdAt)}</p>
+      </div>
+      <div className="my-8">
+        <img
+          className="rounded-xl w-full"
+          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+          alt="card-image"
+        />
+      </div>
+      <div className="content">
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
