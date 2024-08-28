c2c0ace8fd65ac0eb26477e49ab8861859aeb6e9
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-27 19:00:12 -0500

    added h styles

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,6 +1,6 @@
 const Badge = ({ category }) => {
   return (
-    <div className="text-[#4b6bfb98] rounded-md bg-[#2938800d] px-[10px] py-1 w-fit text-xs font-semibold">
+    <div className="text-[#4b6bfb98] rounded-md bg-[#2938800d] px-[10px] py-1 w-fit text-xs font-semibold h-6">
       {category}
     </div>
   );
